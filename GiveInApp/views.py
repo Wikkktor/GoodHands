3df844a22955b2adc839227109057d9efac8a973
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-11 21:40:58 +0100

    MainPage amount of institutions and dontations displaying

--- GiveInApp/views.py
+++ GiveInApp/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from django.db.models import Sum
+from GiveInApp.models import Institution, Donation
 
 
 class MainPageView(View):
     def get(self, request):
-        context = render(request, 'base.html')
+        institutions = len(Institution.objects.all())
+        donations = Donation.objects.aggregate(Sum('quantity'))
+        context = render(request, 'base.html', {'institutions': institutions, 'donations': donations['quantity__sum']})
         return context
 
 
