49162e9969602e68d30221b18890dc5fa8f1737e
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-11 16:58:55 +0100

    Adding models; institution and donation

--- GiveInApp/models.py
+++ GiveInApp/models.py
@@ -1,7 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Institution(models.Model):
+    CHOICES = [
+        (1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lokalna')
+    ]
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.IntegerField(default=1, choices=CHOICES, max_length=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
