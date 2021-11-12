cc94d95b2f8aabb48e0cf12b16ab37b08c9a1ee1
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-12 13:41:06 +0100

    Administration panel with link

--- GiveInApp/admin.py
+++ GiveInApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from GiveInApp import models as m
 # Register your models here.
+
+admin.site.register(m.Category)
+admin.site.register(m.Institution)
+admin.site.register(m.Donation)
+
--- GoodHands/urls.py
+++ GoodHands/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from GiveInApp import views as v
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', v.MainPageView.as_view(), name='main_page'),
     path('accounts/login/', v.LoginView.as_view(), name='login'),
     path('accounts/register/', v.RegisterView.as_view(), name='register'),
